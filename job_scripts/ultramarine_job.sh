0320bcdac921fa010623af790fd989a7657e47eb
Memin Seeker <muhammed.arayici@boun.edu.tr>
2024-08-22 02:13:44 +0300

    fix: update job scripts for Ultramarine and move it

--- job_scripts/ultramarine_job.sh
+++ job_scripts/ultramarine_job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=Llama3.1-8B_4bit_unknown_seq_64rank_12batch_16grad
+#SBATCH --job-name=Llama3.1-8B_4bit_2048_seq_8rank_10batch_4grad
 #SBATCH --output=outs/%j.log
 #SBATCH --container-image ghcr.io\#bouncmpe/cuda-python3
 #SBATCH --time=3-00:00:00
@@ -19,8 +19,10 @@ fi
 
 echo "Downloading the model..."
 tune download meta-llama/Meta-Llama-3.1-8B-Instruct --output-dir /tmp/Meta-Llama-3.1-8B-Instruct --ignore-patterns "original/consolidated.00.pth" --hf-token $HF_TOKEN
-echo "Model downloaded to /tmp/Meta-Llama-3.1-8B-Instruct"
+echo "Model downloaded to ~/Meta-Llama-3.1-8B-Instruct"
 
 echo "Running the fine-tuning..."
-tune run lora_finetune_single_device --config llama3_1/8B_qlora_single_device
+tune run lora_finetune_single_device --config ~/torchtune/recipes/configs/llama3_1/8B_qlora_single_device.yaml
 echo "Fine-tuning completed."
+
+python3 -c "import torch; used_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3); print(f'Used memory: {used_memory} GB')"
